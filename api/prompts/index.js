c4f97e65dccc0a9c5bb3221d5ec823e4530fffdc
Lude397 <ludericksamba@gmail.com>
2026-02-23 22:56:54 +0100

    Delete api/prompts directory

--- api/prompts/index.js
+++
@@ -1,7 +0,0 @@
-export { definition_projet } from './definition_projet.js';
-export { orientation_solution } from './orientation_solution.js';
-export { formulation_solution } from './formulation_solution.js';
-export { design_thinking } from './design_thinking.js';
-export { business_model } from './business_model.js';
-export { lean_startup } from './lean_startup.js';
-export { agile } from './agile.js';
